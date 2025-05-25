b336d81dfec6f77298ad31e3af60b40eff326bfb
Audrey Boucher <audrey@abtasty.com>
2025-05-25 12:48:43 +0200

    init: test + lint coverage on commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders component', () => {
-  const { getByRole } = render(<App />);
-  expect(getByRole('main')).toBeInTheDocument();
+describe('App', () => {
+  test('renders component', () => {
+    const { getByRole } = render(<App />);
+    expect(getByRole('main')).toBeInTheDocument();
+  });
 });
