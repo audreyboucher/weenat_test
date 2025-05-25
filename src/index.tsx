4138180df8f0abc1ca815b123ea1e25069934db5
Audrey Boucher <audrey@abtasty.com>
2025-05-25 11:14:25 +0200

    init: add normalize + set global styles and variables

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 import App from './App';
-import './index.css';
 
 const root = createRoot(
   document.getElementById('root') as HTMLElement
