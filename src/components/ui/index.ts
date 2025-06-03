ed14e5d5059e849e28caece7d6427596a6e8922b
Audrey Boucher <audrey@abtasty.com>
2025-06-03 12:32:58 +0200

    component/MonthSwitch: update UI components exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,5 @@
-export { default as Button, Theme as ButtonType } from './Button/Button';
+export { default as Button, Theme as ButtonType, Variant as ButtonVariant } from './Button/Button';
 export { default as Input } from './Input/Input';
 export { default as Icon } from './Icon/Icon';
+export { default as Select } from './Select/Select';
+export { default as DatePicker } from './DatePicker/DatePicker';
